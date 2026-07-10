efec31b5c4abe6d17856352ec63ba1e2ab9e7ec7
LJP-Zer0 <linkyns@aol.com>
2026-07-10 09:56:02 -0400

    changed about page

--- components/about-page-component.js
+++ components/about-page-component.js
@@ -1,10 +1,12 @@
-export default {
-  name: 'about-page-component',
+export default {  name: 'about-page-component',
   template: /* html */ `
     <section class="container py-4">
       <h1>About List Keeper</h1>
       <p>List Keeper is a simple app for tracking helpful lists and useful notes. It helps people keep their information calm and easy to find.</p>
-      <p>For example, Bobby's mom can save shopping lists, event plans, and reminders in one place, then open each item to see details or notes.</p>
+      <h3>About the Creator!</h3>
+      <p>Hi, I'm Linkyn Peeples, and this is my first ever app and coding project, it's pretty basic, but it's mine.</p>
+      <p>I'm a self-taught developer, and I love to learn new things. I hope to continue to improve my skills and create more apps in the future.</p>
+      <p>Thanks for checking out my app!</p>
     </section>
-  `,
-};
+  `
+  };
