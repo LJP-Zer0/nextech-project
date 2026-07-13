a6dcd50bf8654897318e047216b8a640ceaf0662
LJP-Zer0 <linkyns@aol.com>
2026-07-13 11:45:47 -0400

    Changed about me

--- components/about-page-component.js
+++ components/about-page-component.js
@@ -1,16 +1,12 @@
 export default {  name: 'about-page-component',
   template: /* html */ `
     <section class="container py-4">
-      <div class="about-layout">
-        <div class="about-text">
-          <h1>About List Keeper</h1>
-          <p>List Keeper is a simple app for tracking helpful lists and useful notes. It helps people keep their information calm and easy to find.</p>
-          <h3>About the Creator!</h3>
-          <p>So, about me. Hi! I'm Linkyn Peeples, a beginner developer and this is my first ever coding project. I'm a Boy Scout with Troop 232 in Indiana. I love creating things, especially LEGO sets, those are amazing. </p>
-        </div>
-        <img class = "me" src="Creator.png" height = 190; justify-content: center; align-items: left;>
-      </div>
-
+      <h1>About List Keeper</h1>
+      <p>List Keeper is a simple app for tracking helpful lists and useful notes. It helps people keep their information calm and easy to find.</p>
+      <h3>About the Creator!</h3>
+      <p>So, about me. Hi! I'm Linkyn Peeples, a beginner developer and this is my first ever coding project. I'm a Boy Scout with Troop 232 in Indiana. I love creating things, especially LEGO sets, those are amazing I also love fantasy, dragons, ninja, wizards that sort of stuff. Anyway, thank you for visiting List Keeper! </p>
+    <img src="Creator.png" alt="Linkyn Peeples" class="img-fluid rounded mb-3" style="max-width: 200px;">
+    
     </section>
   `
   };
